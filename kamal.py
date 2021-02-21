dac968e683af1d139c403ebe7ab99c9e46009c46
kamalakar98 <79035758+kamalakar98@users.noreply.github.com>
2021-02-21 12:48:14 +0530

    Add files via upload

---
+++ kamal.py
@@ -0,0 +1,29 @@
+fruits=["fig","lemon","banana"]
+numbers=[1,2,3]
+words={"first word":"banana","second word":"hi"}
+char=("a","b","c")
+
+print(fruits)
+print(numbers)
+print(words)
+print(char)
+
+output:
+['fig', 'lemon', 'banana']
+[1, 2, 3]
+{'first word': 'banana', 'second word': 'hi'}
+('a', 'b', 'c')
+
+
+x=3
+y=4.5
+z=2+3j
+
+print(x,"is of type",type(x))
+print(y,"is of type",type(y))
+print(z,"is of type",type(z))
+
+output:
+3 is of type <class 'int'>
+4.5 is of type <class 'float'>
+(2+3j) is of type <class 'complex'>
